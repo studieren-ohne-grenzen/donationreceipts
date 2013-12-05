5062dc65a37f911e49150fff73a9d612d25a73fc
Olaf Buddenhagen <antrik@digitalcourage.de>
2013-12-05 23:56:24 +0100

    Jahresbescheinigungen: create merged document in multiple passes
    Avoid command line length overflow when we have many receipts, by doing
    the merge in several smaller batches.
    
    This is done in an iterative process, with as many intermediate passes
    as necessary.

--- CRM/Donationreceipts/Page/Jahresbescheinigungen.php
+++ CRM/Donationreceipts/Page/Jahresbescheinigungen.php
@@ -54,18 +54,61 @@ class CRM_Donationreceipts_Page_Jahresbescheinigungen extends CRM_Core_Page {
     if (!empty($result)) {
       $this->assign("have_result", true);
 
-      // $result is already sorted by contact_id
-      $docs = array_flip(array_map(function ($elem) { return $elem['filename']; }, $result));
+      /*
+       * Create a merged document containing all the individual receipts.
+       *
+       * The actual merging is done by invoking an external tool (pdftk)
+       * with all the individual receipt documents as command line parameters.
+       *
+       * However, as most operating systems (except GNU Hurd) have a limited maximal command line length,
+       * trying to merge all documents in one go would cause an overflow if we have a lot of receipts.
+       * Thus, we have to merge them in smaller batches first, before combining these into the final result.
+       *
+       * We do this in an iterative process, with potentially multiple intermediate levels.
+       * This is most probably overkill -- but better safe than sorry :-)
+       */
 
-      $config =& CRM_Core_Config::singleton( );
+      define('BATCH_SIZE', 1000); // With a typical command line length limit of 64 KiB, this should give us enough leeway...
 
-      // set up file names
-      $baseName = CRM_Utils_File::makeFileName("Jahresbescheinigungen-$year.pdf");
-      $outFile = $config->customFileUploadDir . "$baseName";
+      $tempDir = CRM_Utils_File::tempdir('donationreceipts-');
 
-      $cmd = "cd " . $config->customFileUploadDir . "; pdftk " . join(" ", array_keys($docs)) . " cat output $outFile";
+      $config = CRM_Core_Config::singleton();
 
-      system($cmd);
+      // In the first pass, we start with the individual receipt documents we just generated.
+      $inFiles = array_map(function ($elem) { return $elem['filename']; }, $result);
+      $inDir = $config->customFileUploadDir;
+
+      for ($pass = 1, $isFinalPass = false; !$isFinalPass; ++$pass) { // Do at least one pass; and further ones as necessary.
+        $batches = array_chunk($inFiles, BATCH_SIZE);
+        $isFinalPass = !(count($batches) > 1); // If the current input files fit into a single batch, we will be done after this pass; otherwise, we need further passes.
+
+        $outFiles = array();
+
+        foreach ($batches as $batchID => $filesInBatch) {
+          // If this is the final pass, the current output will be the actual result file to save; otherwise, we generate temporary files for further merging.
+          $outDir = $isFinalPass ? $config->customFileUploadDir : $tempDir;
+          $baseName = $isFinalPass ? CRM_Utils_File::makeFileName("Jahresbescheinigungen-$year.pdf") : "$pass-$batchID.pdf";
+          $outFile = $outDir . $baseName;
+
+          $inputs = join(" ", $filesInBatch);
+          system("cd $inDir && pdftk $inputs cat output $outFile");
+
+          $outFiles[] = $baseName;
+
+          // If our inputs are temporary results from the previous pass, we can drop them now.
+          if ($inDir == $tempDir) {
+            foreach ($filesInBatch as $file) {
+              unlink($tempDir . $file);
+            }
+          }
+        }
+
+        // Outputs from this pass are inputs for next one.
+        $inFiles = $outFiles;
+        $inDir = $tempDir;
+      } // for ($pass)
+
+      rmdir($tempDir);
 
       if (file_exists($outFile)) {
         $session = CRM_Core_Session::singleton();
